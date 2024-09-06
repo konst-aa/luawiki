a099e23f4a0a5f883b11443c964140cd09e7b7c0
Konstantin Astafurov <konstantin.astafurov@gmail.com>
2024-09-06 00:29:00 -0400

    i have no idea what im doing

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Luawiki
-My favorite features of [Vimwiki](https://github.com/vimwiki/vimwiki) + other stuff I come up with
+My favorite parts of [Vimwiki](https://github.com/vimwiki/vimwiki) + other stuff I come up with
 
 
 As of 2024-09-06 I no longer us Vimwiki. It was a great ride
--- lua/luawiki.lua
+++ lua/luawiki.lua
@@ -1,66 +1,17 @@
-local function ind(s, i)
-    return string.sub(s, i, i)
-end
-local function starts(s, pre)
-    print(string.sub(s,1,string.len(pre)))
-   return string.sub(s,1,string.len(pre))==pre
-end
-local function ends(s, post)
-    return string.sub(s, -string.len(post))==post
-end
+local M = {}
 
-Depth = {}
+function M.setup(user_config)
+    local default_config = {
+    }
 
-local function link()
-    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-    local line = vim.api.nvim_buf_get_lines(0, row-1, row, {})[1]
-    local s = col
-    local e = col+1
+    M.config = vim.tbl_deep_extend("force", default_config, user_config or {})
 
-    while s > 0 and ind(line, s) ~= ' ' and ind(line, e) ~= '[' do
-        s = s - 1
-    end
-    while e <= string.len(line) and ind(line, e) ~= ' ' and ind(line, e) ~= ')' do
-        e = e + 1
-    end
-
-    local block = string.sub(line, s, e)
-
-    if starts(block, " ") then
-        block = string.sub(block, 2, string.len(block))
-    end
-    if ends(block, " ") then
-        block = string.sub(block, 1, string.len(block) - 1)
-    end
-
-    if (starts(block, "[") and ends(block, ')')) then
-        local t = 1
-        while ind(block, t) ~= '(' do
-            t = t + 1
-        end
-
-        local name = string.sub(block, t+1, string.len(block) - 1)
-        -- print(#Depth)
-        table.insert(Depth, vim.api.nvim_buf_get_name(0))
-        vim.cmd("e " .. name)
-    elseif (string.len(block) > 0) then
-        vim.api.nvim_buf_set_text(0, row - 1, s, row - 1, s, { "[" })
-        vim.api.nvim_buf_set_text(0, row - 1, e, row - 1, e, { "](" .. block .. ".md)" })
-    end
-end
-local function goback()
-    if #Depth > 0 then
-        vim.cmd("e " .. table.remove(Depth))
-    end
+    require("luawiki_require")
 end
 
 
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = {"markdown"},
-    callback = function()
-        vim.schedule(function()
-            vim.keymap.set('n', '<CR>', link)
-            vim.keymap.set('n', '<BS>', goback)
-        end)
-    end
-})
+if not pcall(debug.getlocal, 4, 1) then
+    M.setup()
+end
+
+return M
